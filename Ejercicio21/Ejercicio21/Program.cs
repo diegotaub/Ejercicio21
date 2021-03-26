8ce7ff3fb6356ec499023df08087ac89510a8b20
diego.taub <diego.taub@accenture.com>
2021-03-25 21:13:01 -0300

    Ejercicio21

---
+++ Ejercicio21/Ejercicio21/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"La fecha actual es {DateTime.Now}");
+            Console.ReadKey();
+        }
+    }
+}
